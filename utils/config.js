d6c751a0e81b317f094219f640ccbeac6854a23c
Nasantogtokh Amarsaikhan <nasaa@Nasantogtokhs-iMac.local>
2020-11-06 09:48:47 +0800

    Configured Service Account for tweet-api

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,10 @@
 /* Firestore admin SDK config */
-const firebase = require("firebase");
+const firebase = require("firebase-admin");
 require("firebase/firestore");
+var serviceAccount = require("../twitter-app-a08a4-firebase-adminsdk-ftwdd-37edc472fc.json");
 const firebaseConfig = {
-  apiKey: "AIzaSyCdieC7ProvSDITn-PE7bCiiL75QS7ceZI",
-  authDomain: "twitter-app-a08a4.firebaseapp.com",
-  databaseURL: "https://twitter-app-a08a4.firebaseio.com",
-  projectId: "twitter-app-a08a4",
-  storageBucket: "twitter-app-a08a4.appspot.com",
-  messagingSenderId: "385100571391",
-  appId: "1:385100571391:web:b6d833952f0a34527e22ca"
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://twitter-app-a08a4.firebaseio.com"
 };
 firebase.initializeApp(firebaseConfig);
 exports.db = firebase.firestore();
