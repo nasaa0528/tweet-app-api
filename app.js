c362c9708833725d944ce1cee45e6a94f3782a8d
Nasantogtokh Amarsaikhan <nasaa@Nasantogtokhs-iMac.local>
2020-11-09 09:57:17 +0800

    CORS added

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const usersRouter = require('./users/router');
 const postsRouter = require('./posts/router');
 const followRouter = require('./users/follows/router');
+const cors = require('cors');
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 const router = express.Router();
 
 /* Middlewares */
+app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 app.use('/users', usersRouter);
